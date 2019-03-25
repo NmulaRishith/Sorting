cca86b7c7f73e18ba1ffa0a95e6e2680813e5af3
nr2678 <nr2678@CSELAB01-30.bennett.edu.in>
2019-03-25 16:41:16 +0530

    Bubble Sort

---
+++ src/sorting/BubbeSort.java
@@ -0,0 +1,15 @@
+package sorting;
+import java.util.Arrays;
+public class BubbeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int data[]={4,-5,2,6,1};
+		Arrays.sort(data);
+		for (int c: data){
+			System.out.println(c);
+		}
+
+	}
+
+}
